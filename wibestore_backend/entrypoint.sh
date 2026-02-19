74e2141b107a995e7d77be88f3963e774d8ff32b
Amirxon Baxtiyorov <amrxonn.baxtiyorov@gmail.com>
2026-02-19 15:58:13 +0500

    Auto-generate migrations in entrypoint because they are missing

--- wibestore_backend/entrypoint.sh
+++ wibestore_backend/entrypoint.sh
@@ -7,6 +7,10 @@ set -e
 
 echo "==> WibeStore Entrypoint Started"
 
+echo "==> Making migrations..."
+python manage.py makemigrations accounts --noinput
+python manage.py makemigrations --noinput
+
 echo "==> Running migrations..."
 python manage.py migrate --noinput
 
